163016d713f98736a8b80999428df861c8230706
Indraswara <indrswara@gmail.com>
2025-11-07 23:09:12 +0700

    feat: checkpoint-1

---
+++ content/education.js
@@ -0,0 +1,14 @@
+window.EDUCATION_ITEMS = [
+  {
+    title: "Bachelor of Computer Science",
+    date: "2016 - 2020",
+    description: "Graduated from XYZ University with focus on software engineering.",
+    tags: ["Computer Science", "University"],
+  },
+  {
+    title: "Web Development Bootcamp",
+    date: "2016",
+    description: "Intensive bootcamp focusing on full-stack web development.",
+    tags: ["Bootcamp", "Web Development"],
+  },
+]
---
+++ content/home.js
@@ -0,0 +1,5 @@
+window.HOME_CONTENT = {
+  intro: [
+    "Hello, you just came to my personal website. My name is Indraswara, you can call me Indra.",
+  ],
+}
