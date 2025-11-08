2c5ab8d25a33f2597b0b1f29be95808e4af07809
Indraswara <indrswara@gmail.com>
2025-11-08 16:01:20 +0700

    feat: update experience

--- content/experience.js
+++ content/experience.js
@@ -5,4 +5,10 @@ window.EXPERIENCE_ITEMS = [
     description: "Developing a mobile scraper and some reverse engineering tasks",
     tags: ["JavaScript", "Python", "Burpsuite"],
   },
+  {
+    title: "Laboratory Assistant at SISTER ITB",
+    date: "08/2024 - Present",
+    description: "Assisting students in understanding networking concepts and practical lab work.",
+    tags: ["Computer Networking", "Operating Systems", "Linux", "Docker", "Virtualization", "Distributed Systems", "Computer Architecture"],
+  }
 ]
