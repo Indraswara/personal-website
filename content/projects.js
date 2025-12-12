1edf5a9ac6d2442114a2ad8470c58a83bf59881e
Indraswara <indrswara@gmail.com>
2025-12-12 17:00:03 +0700

    feat: update project

--- content/projects.js
+++ content/projects.js
@@ -4,6 +4,20 @@ window.PROJECT_ITEMS = [
     date: "2024",
     description: "A simple, plain text personal website with easy content management.",
     tags: ["HTML", "CSS", "JavaScript", "Docker"],
-    link: "https://github.com/indraswara/personal-website",
+    link: "https://short.indraswara.me/personal-web",
   },
+  {
+    title: "Operating System", 
+    date: "2024", 
+    description: "A simple operating system built from scratch using C and Assembly language, featuring basic functionalities such as file management, process scheduling, and memory management.",
+    tags: ["C", "Assembly", "Operating System"], 
+    link: "https://short.indraswara.me/OS"
+  }, 
+  {
+    title: "Simple Cryptocraphic Chat", 
+    date: "2025",
+    description: "A secure chat application implementing end-to-end encryption using ECC (Elliptic Curve Cryptography) to ensure private communication between users.",
+    tags: ["Cryptography", "ECC", "Chat Application", "Python", "WebSocket"],
+    link: "https://sigmachat.indraswara.me"
+  }
 ]
